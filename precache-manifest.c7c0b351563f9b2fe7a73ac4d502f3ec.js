b0f3eb27491f5dbe84bf5535df6922a5959df296
avatar92 <adelbensalem1992@gmail.com>
2019-06-26 00:56:33 +0200

    Updates

---
+++ precache-manifest.c7c0b351563f9b2fe7a73ac4d502f3ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd7761383c244dd37f90928981810f64",
+    "url": "/HOC/index.html"
+  },
+  {
+    "revision": "9d678e2e9842e3eb9277",
+    "url": "/HOC/static/css/main.3675ddb5.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/HOC/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "9d678e2e9842e3eb9277",
+    "url": "/HOC/static/js/main.049df33e.chunk.js"
+  },
+  {
+    "revision": "5a84c408d01851539b04",
+    "url": "/HOC/static/js/runtime~main.74158f72.js"
+  }
+]);
